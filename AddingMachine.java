f0cfa5744836e1bc6782368c01e557e08a906363
akong3 <audreykong@Audreys-MacBook-Pro.local>
2019-10-09 23:34:35 -0700

    updated AddingMachine

--- AddingMachine.java
+++ AddingMachine.java
@@ -1,30 +1,37 @@
+// NAME:  Audrey Kong
+// Assignment #2
+// Assignment #2
 package cse360assign2;
 
 public class AddingMachine {
 
 	private int total;
-	
+	private String history;
+
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
-	
+
 	public int getTotal () {
-		return 0;
+		return total;
 	}
-	
+
 	public void add (int value) {
-		
+		total = total + value;
+		history = history + total + " " +  "+" + " "+ value + "\n";
 	}
-	
+
 	public void subtract (int value) {
-		
+		total = total - value;
+		history = history + total + " " + "-" + " " + value + "\n";
+
 	}
-		
+
 	public String toString () {
-		return "";
+		return history;
 	}
 
 	public void clear() {
-	
+
 	}
 }
